9192eb9d69d2f0c329a9a02a843c83ecd8b36599
Joe Aguado <aguado.joe@gmail.com>
2024-06-04 13:30:44 -0500

    added: hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,10 +3,13 @@ import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 import ShimmerButton from './ui/ShimmerButton'
 import NormalButton from './ui/NormalButton'
+import { GiTexas } from "react-icons/gi";
+import image from "../public/mobileHero.png"
+import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36'>
+    <div className='pb-20 pt-24'>
         <div>
             <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
             <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>
@@ -18,7 +21,11 @@ const Hero = () => {
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
             </div>
 
-            <div className='flex justify-center relative my-20 z-10'>
+            <div className='flex flex-col lg:flex-row lg:flex-row-reverse relative z-10 items-center'>
+            <Image src={image} alt='Hero Image' className='max-w-[300px] rounded-[20px] border border-slate-500 '/>
+
+<div className='flex relative my-4 z-10'>
+
                 <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                     <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>work with the most trusted</h2>
 
@@ -27,9 +34,14 @@ const Hero = () => {
                         words="Bridging the Gap Between Business and Technology"
                         />
 
-                        <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-                            Hello, I&apos;m Joe, a Web Developer based out of Dallas, TX. 
+                        <div className='flex items-center mb-4'>
+                        <p className='text-center lg:tracking-wider text-sm lg:text-lg xlg:text-2xl mb-1'>
+                            Hello, I&apos;m Joe, a Web Developer based out of Dallas, TX
                         </p>
+                        <GiTexas className='text-[18px] md:text-[25px] ml-2'
+                        size='' />
+                        </div>
+
                         <div className='flex gap-8'>
                         <NormalButton
                         title="View Work" />
@@ -38,6 +50,10 @@ const Hero = () => {
                         </div>
                 </div>
             </div>
+
+            </div>
+
+            
     </div>
   )
 }
--- components/ui/NormalButton.tsx
+++ components/ui/NormalButton.tsx
@@ -5,7 +5,7 @@ const NormalButton = ( {title}: { title : any } ) => {
     return (
 
         // Button code
-        <button className="inline-flex h-12 items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 tracking-widest">
+        <button className="inline-flex h-12 items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,75%,#000103)] bg-[length:200%_100%] px-6 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 tracking-widest">
           {title}
         </button>
   
