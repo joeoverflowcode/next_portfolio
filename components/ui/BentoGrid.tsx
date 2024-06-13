2166fa400c4d94fa94563d3aa5462ecff78a720b
Joe Aguado <aguado.joe@gmail.com>
2024-06-12 19:43:53 -0500

    added: resume button

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -2,6 +2,9 @@ import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
 import {GlobeDemo} from './GridGlobe'
 import Image from 'next/image'
+import NormalButton from "./NormalButton";
+import ShimmerButton from "./ShimmerButton";
+import ResumeButton from './ResumeButton'
 
 export const BentoGrid = ({
   className,
@@ -57,7 +60,7 @@ export const BentoGridItem = ({
       }}
     >
 
-      <div className={`${id === 6 && 'flex justify-center'} h-full`}>
+      <div className={`${id === 6 && 'flex justify-center flex-col items-center'} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
@@ -80,14 +83,14 @@ export const BentoGridItem = ({
         </div>
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
+            <div className="absolute z-50 flex items-center justify-center text-center text-white font-bold" />
           </BackgroundGradientAnimation>
         )}
 
         <div className={cn(
           titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 '
         )}>
-          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+          <div className="font-sans font-extralight md:max-w-32 md:text-sm text-xs text-[#C1C2D3] z-10">
             {description}
           </div>
           <div className="font-sans font-bold text-lg lg:text-2xl max-w-96 z-10">
@@ -119,8 +122,12 @@ export const BentoGridItem = ({
         )}
 
         {id === 6 && (
-          <div className="mt-5 relative">
-            <div className={`absolute -bottom-5 right-0`}>
+          <div className="flex flex-col items-center relative text-center">
+            <div className={`absolute mt-1 items-center text-center`}>
+
+            <ResumeButton 
+            
+            />
 
             </div>
 
