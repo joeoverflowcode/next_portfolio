7438fc7f70f994341e179397229916c2411a094a
Joe Aguado <aguado.joe@gmail.com>
2024-06-28 12:40:10 -0500

    debugging: card reference location updated

--- components/ui/RecentProjects.tsx
+++ components/ui/RecentProjects.tsx
@@ -15,7 +15,7 @@ const RecentProjects = () => {
                 <div key={id} className='sm:h-[41rem] lg:min-h-[32.5rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
                     
                      <PinContainer title={link} href={link}>
-                        <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[30vh] sm:h-[40vh] mb-10'>
+                        <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[40vh] sm:h-[40vh] mb-10'>
                             <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                                 <img src='/bg.png' alt='bg-img'/>
                             </div>
